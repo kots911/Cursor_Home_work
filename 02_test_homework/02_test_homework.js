1e545c3a0188e04021d42ee9a8a80c54a6ed9102
kots911 <85323888+kots911@users.noreply.github.com>
2021-06-07 18:42:07 +0300

    new changes to my homework

--- 02_test_homework/02_test_homework.js
+++ 02_test_homework/02_test_homework.js
@@ -1,17 +1,36 @@
 
 let numN;
 let numM;
-
+let sum;
+let even;
 do {
-  numN = +prompt("Введите первое целое чилое:");
-  } while ((isNaN(numN) || numN !== parseInt(numN) ));
+  numN = +prompt("Введите первое целое чилое:","");
+  } while ((isNaN(numN) || numN !== parseInt(numN) || (!numN) ));
   
-  do { 
-    numM = +prompt("Введите второе целое чилое:");
+do { 
+    numM = +prompt("Введите второе целое чилое:","");
     
-  } while ((isNaN(numM) || numM !== parseInt(numM)));
+  } while ( (isNaN(numM) || numM !== parseInt(numM) || (!numN) ));
 
   console.log(numN);
   console.log(numM);
   
-  
+  even = confirm("Пропускать парные числа?");
+  console.log(even);
+  if (even) {
+    sum = 0;
+    for (let i = numN;  i <= numM; i++) {
+      if ( i % 2 == 0)  {
+        continue;
+      }
+      sum += i;
+  }
+  }
+    else {
+      sum = 0;
+      for ( let i = numN; i <= numM; i++) {
+        sum = sum + i;
+      }
+     
+};
+alert("Сумма всех чисел в цикле:" + sum);
