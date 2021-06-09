7fbd1fe884302f1dc3f5fe836a9e3d2b786249e1
kots911 <85323888+kots911@users.noreply.github.com>
2021-06-09 14:34:44 +0300

    commit on master

--- 01_test_homework/main.js
+++ 01_test_homework/main.js
@@ -5,8 +5,8 @@ const numOfProducts = 3;
 document.write('<pre>');
 document.writeln( "Минимальная цена: " + Math.min(pricePhone, priceLaptop, pricePC));
 document.writeln( "Максимальная цена: " + Math.max(pricePhone, priceLaptop, pricePC));
-const sum = (pricePhone + priceLaptop + pricePC);
-document.writeln("Сумма всех товаров: " + sum);
+const sumOfPrices = (pricePhone + priceLaptop + pricePC);
+document.writeln("Сумма всех товаров: " + sumOfPrices);
 
 const pricePhoneFloor = Math.floor(pricePhone);
 const priceLaptopFloor = Math.floor(priceLaptop);
@@ -14,28 +14,28 @@ const pricePCFloor = Math.floor(pricePC);
 const sumFloor = (pricePhoneFloor + priceLaptopFloor + pricePCFloor);
 
 document.writeln( "Округленная сумма товаров: " + sumFloor);
-const sumFloorRound = Math.round(sum/100)*100;
+const sumFloorRound = Math.round(sumOfPrices/100)*100;
 document.writeln( "Сумма товаров, округленная до сотен: " + sumFloorRound);
 
-const boolean = (sumFloorRound % 2 === 0);
+const boolean = sumFloorRound % 2 === 0;
 
 document.writeln("Если сумма всех товаров является парным числом - true=" + boolean);
 
-const pay = (500 - sum);
+const pay = (500 - sumOfPrices);
 document.writeln ("Сдача с 500грн при покупке всех товаров:" + pay);
-const sum_02 = (+(sum / numOfProducts).toFixed(2));
+const sum_02 = (+(sumOfPrices / numOfProducts).toFixed(2));
 document.writeln ("Среднее значение цен, округленное:" + sum_02);
 
 const discountRandom = (Math.random() * 100);
 const clientDiscountRandom = (discountRandom).toFixed(2);
 document.writeln ("Случайный процент скидки клиенту:" + clientDiscountRandom + "%");
 
-const priseSumDiscount = (sum / 100 * clientDiscountRandom);
-const purchasePriceSum = (sum / 2);
-const clearPrice = (sum - priseSumDiscount - purchasePriceSum);
+const priseSumDiscount = (sumOfPrices / 100 * clientDiscountRandom);
+const purchasePriceSum = (sumOfPrices / 2);
+const clearPrice = (sumOfPrices - priseSumDiscount - purchasePriceSum);
 
 document.writeln ("Чистый доход:" + clearPrice);
-const  payPriseSumDiscount = (+(sum - priseSumDiscount).toFixed(2));
+const  payPriseSumDiscount = (+(sumOfPrices - priseSumDiscount).toFixed(2));
 document.writeln ("Сумма к оплате после скидки:" + payPriseSumDiscount);
 
 
